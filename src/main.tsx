024843e62efd2da9590d5092014247be7fd6d719
Patrick Liu <zeldalink0515@gmail.com>
2021-09-02 17:05:35 +0800

    Update machinat and profile codes

--- src/main.tsx
+++ src/main.tsx
@@ -19,60 +19,70 @@ const main = (
 ): void => {
   const event$ = merge<AppEventContext>(chat$, timer$);
 
-  event$.pipe(
-    tap<AppEventContext>(
-      makeContainer({
-        deps: [Machinat.Bot, Script.Processor, Messenger.Profiler] as const,
-      })(
-        (bot, scriptProcessor, messengerProfiler) =>
-          async (context: AppEventContext) => {
-            const { event } = context;
-            const { channel } = event;
-            if (!channel) {
-              return;
-            }
-
-            let runtime = await scriptProcessor.continue(channel, context);
-            if (!runtime) {
-              let timezone: undefined | number;
-              if (event.platform === 'messenger' && event.user) {
-                ({ timezone } = await messengerProfiler.getUserProfile(
-                  event.user
-                ));
+  event$
+    .pipe(
+      tap<AppEventContext>(
+        makeContainer({
+          deps: [Machinat.Bot, Script.Processor, Messenger.Profiler] as const,
+        })(
+          (bot, scriptProcessor, messengerProfiler) =>
+            async (context: AppEventContext) => {
+              const { event } = context;
+              const { channel } = event;
+              if (!channel) {
+                return;
               }
 
-              runtime = await scriptProcessor.start(channel, Pomodoro, {
-                params: { timezone },
-              });
-            }
+              let runtime = await scriptProcessor.continue(channel, context);
+              if (!runtime) {
+                let timezone: undefined | number;
 
-            await bot.render(channel, runtime.output());
-          }
+                if (event.platform === 'messenger' && event.user) {
+                  const profile = await messengerProfiler.getUserProfile(
+                    event.user
+                  );
+                  timezone = profile?.timezone;
+                }
+
+                runtime = await scriptProcessor.start(channel, Pomodoro, {
+                  params: { timezone },
+                });
+              }
+
+              await bot.render(channel, runtime.output());
+            }
+        )
       )
     )
-  );
+    .catch(console.error);
 
-  chat$.pipe(
-    filter(
-      (
-        ctx
-      ): ctx is MessengerEventContext & { event: { category: 'message' } } =>
-        ctx.platform === 'messenger' && ctx.event.category === 'message'
-    ),
-    tap(async ({ reply }) => {
-      await reply(<MarkSeen />);
-    })
-  );
+  chat$
+    .pipe(
+      filter(
+        (
+          ctx
+        ): ctx is MessengerEventContext & { event: { category: 'message' } } =>
+          ctx.platform === 'messenger' && ctx.event.category === 'message'
+      ),
+      tap(async ({ reply }) => {
+        await reply(<MarkSeen />);
+      })
+    )
+    .catch(console.error);
 
-  chat$.pipe(
-    filter(
-      (ctx): ctx is ChatEventContext & { event: { type: 'callback_query' } } =>
-        ctx.event.type === 'callback_query'
-    ),
-    tap(async ({ event, reply }) => {
-      await reply(<AnswerCallbackQuery queryId={event.queryId} />);
-    })
-  );
+  chat$
+    .pipe(
+      filter(
+        (
+          ctx
+        ): ctx is ChatEventContext & { event: { type: 'callback_query' } } =>
+          ctx.event.type === 'callback_query'
+      ),
+      tap(async ({ event, reply }) => {
+        await reply(<AnswerCallbackQuery queryId={event.queryId} />);
+      })
+    )
+    .catch(console.error);
 };
 
 export default main;
