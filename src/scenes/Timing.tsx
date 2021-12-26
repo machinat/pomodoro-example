3bbe07de2bf2a11801308349b713acd5f3cb3ed7
Patrick Liu <zeldalink0515@gmail.com>
2021-12-26 12:55:27 +0800

    fix: run prettier

--- src/scenes/Timing.tsx
+++ src/scenes/Timing.tsx
@@ -1,7 +1,15 @@
 import ordinal from 'ordinal';
 import Machinat from '@machinat/core';
 import { build } from '@machinat/script';
-import { $, IF, THEN, WHILE, PROMPT,EFFECT, RETURN } from '@machinat/script/keywords';
+import {
+  $,
+  IF,
+  THEN,
+  WHILE,
+  PROMPT,
+  EFFECT,
+  RETURN,
+} from '@machinat/script/keywords';
 import TimingCard from '../components/TimingCard';
 import StopingCard from '../components/StopingCard';
 import ReplyBasicActions from '../components/ReplyBasicActions';
@@ -18,7 +26,7 @@ import type {
   PomodoroEventContext,
   AppActionType,
   PomodoroSettings,
-  AppChannel
+  AppChannel,
 } from '../types';
 
 type TimingParams = {
@@ -43,9 +51,10 @@ const PROMPT_WHEN_TIMING = (key: string) => (
   <PROMPT<TimingVars, PomodoroEventContext>
     key={key}
     set={async ({ vars }, { event, intent }) => ({
-        ...vars,
-        action: intent.type,
-        settings: event.type === 'settings_updated'
+      ...vars,
+      action: intent.type,
+      settings:
+        event.type === 'settings_updated'
           ? event.payload.settings
           : vars.settings,
     })}
@@ -75,10 +84,13 @@ export default build<
         time > Date.now() - beginAt.getTime()
       }
     >
-      {({ channel,vars: { settings, action, time, phase, pomodoroNum, beginAt } }) => {
+      {({
+        channel,
+        vars: { settings, action, time, phase, pomodoroNum, beginAt },
+      }) => {
         return (
           <ReplyBasicActions
-          channel={channel as AppChannel}
+            channel={channel as AppChannel}
             action={action}
             settings={settings}
             defaultReply={
@@ -95,44 +107,66 @@ export default build<
       {PROMPT_WHEN_TIMING('wait-timing-up')}
 
       {/* double check for skipping working phase */}
-      <IF<TimingVars> condition={({ vars }) => vars.action === ACTION_SKIP &&  vars.phase === TimingPhase.Working}>
+      <IF<TimingVars>
+        condition={({ vars }) =>
+          vars.action === ACTION_SKIP && vars.phase === TimingPhase.Working
+        }
+      >
         <THEN>
-        {() => <StopingCard>Skip current 🍅?</StopingCard>}
-        {PROMPT_WHEN_TIMING('ask-should-skip')}
+          {() => <StopingCard>Skip current 🍅?</StopingCard>}
+          {PROMPT_WHEN_TIMING('ask-should-skip')}
 
-        <EFFECT<TimingVars> set={({vars}) => ({
-          ...vars,
-          action: vars.action === ACTION_OK ? ACTION_SKIP : vars.action
-        })} />
+          <EFFECT<TimingVars>
+            set={({ vars }) => ({
+              ...vars,
+              action: vars.action === ACTION_OK ? ACTION_SKIP : vars.action,
+            })}
+          />
         </THEN>
       </IF>
     </WHILE>
 
-    {({ vars: {action, phase, pomodoroNum,  settings } }) =>{
-      if (phase!==TimingPhase.Working) {
-        return<p>Break time {action === ACTION_PAUSE ? 'paused' :action === ACTION_SKIP ?'skipped': 'is up'}</p>
+    {({ vars: { action, phase, pomodoroNum, settings } }) => {
+      if (phase !== TimingPhase.Working) {
+        return (
+          <p>
+            Break time{' '}
+            {action === ACTION_PAUSE
+              ? 'paused'
+              : action === ACTION_SKIP
+              ? 'skipped'
+              : 'is up'}
+          </p>
+        );
       }
 
       if (pomodoroNum === settings.pomodoroPerDay) {
-        return <FinishTarget pomodoroTarget={settings.pomodoroPerDay} />
+        return <FinishTarget pomodoroTarget={settings.pomodoroPerDay} />;
       }
 
-      return <p>
-          {ordinal(pomodoroNum)} 🍅 {action === ACTION_PAUSE ? 'paused' :action === ACTION_SKIP ?'skipped' : 'finished'}
+      return (
+        <p>
+          {ordinal(pomodoroNum)} 🍅{' '}
+          {action === ACTION_PAUSE
+            ? 'paused'
+            : action === ACTION_SKIP
+            ? 'skipped'
+            : 'finished'}
         </p>
-
+      );
     }}
 
     <RETURN<TimingVars, TimingReturn>
-      value={({ vars: { beginAt, time, settings, phase , action} }) => ({
+      value={({ vars: { beginAt, time, settings, phase, action } }) => ({
         settings,
         pomodoroRecord:
           phase === TimingPhase.Working && action !== ACTION_PAUSE
             ? [beginAt, new Date()]
             : null,
-        remainingTime: action === ACTION_PAUSE
-          ? Math.max(0, time - (Date.now() - beginAt.getTime()))
-          : 0,
+        remainingTime:
+          action === ACTION_PAUSE
+            ? Math.max(0, time - (Date.now() - beginAt.getTime()))
+            : 0,
       })}
     />
   </$>
